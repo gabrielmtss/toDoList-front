5dfbc9c3a19751709a3ab64fbf69688bb501e66c
Gabriel Matos <g.matos@outlook.com>
2022-01-11 18:17:45 -0300

    cria cadastro

---
+++ src/pages/Cadastro/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import Api from '../../api/api';
+import { useNavigate } from 'react-router-dom';
+
+const Cadastro = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (evento) => {
+    evento.preventDefault();
+    const titulo = evento.target.titulo.value;
+    const descricao = evento.target.descricao.value; 
+    const prioridade = evento.target.prioridade.value;
+    const status = evento.target.status.value;
+    const prazo = evento.target.prazo.value;
+
+    const task = {
+      titulo,
+      descricao,
+      prioridade,
+      status,
+      prazo,
+    }
+
+    const response = await Api.fetchPost(task);
+    const result = await response.json();
+
+    alert(result.message);
+    navigate('/');
+  }
+
+  return (
+    <div className='container'>
+      <div className='card mt-4'>
+        <div className='card-title'>
+          <h3 className='m-3'>Cadastro de tarefas</h3>
+        </div>
+        <div className='card-body'>
+          <form method='POST' onSubmit={handleSubmit}>
+            <div className='row mb-4'>
+              <div className='col-4'>
+                <div className='form-group'>
+                  <label htmlFor='titulo'>Titulo da tarefa:</label>
+                  <input id='titulo' className='form-control' type='text' placeholder='Digite o titulo da tarefa' name='titulo'/>
+                </div>
+              </div>
+              <div className='col-4'>
+                <div className='form-group'>
+                  <label htmlFor='descricao'>Descrição da tarefa:</label>
+                  <input id='descricao' className='form-control' type='text' placeholder='Digite a descrição da tarefa' name='descricao'/>
+                </div>
+              </div>
+              <div className='col-4'>
+                <div className='form-group'>
+                  <label htmlFor='prioridade'>Prioridade da tarefa:</label>
+                  <input id='prioridade' className='form-control' type='text' placeholder='Defina a prioridade da tarefa' name='prioridade'/>
+                </div>
+              </div>
+            </div>
+            <div className='row mb-4'>
+              <div className='col-4'>
+                <div className='form-group'>
+                  <label htmlFor='status'>Status da tarefa:</label>
+                  <input id='status' className='form-control' type='text' placeholder='Digite o status da tarefa' name='status'/>
+                </div>
+              </div>
+              <div className='col-4'>
+                <div className='form-group'>
+                  <label htmlFor='prazo'>Prazo da tarefa:</label>
+                  <input id='prazo' className='form-control' type='text' placeholder='Defina o prazo da tarefa' name='prazo'/>
+                </div>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-4'>
+                <button className='btn btn-success me-2' type='submit'>Enviar</button>
+                <button className='btn btn-outline-primary'>Voltar</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cadastro
