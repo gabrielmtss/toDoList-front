8cea5e1a199388814f3ded808591eecdc9b27731
Gabriel Matos <g.matos@outlook.com>
2022-01-11 18:01:28 -0300

    edita header

--- src/components/shared/Header/index.js
+++ src/components/shared/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-primary w-100">
       <div className="container">
-        <a className="navbar-brand" href="/">Blue Games</a>
+        <a className="navbar-brand" href="/">to-do list</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
