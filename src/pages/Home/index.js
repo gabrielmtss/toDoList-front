9c5a808ce1003fa1cd3fb1945759196705fa2eb8
Gabriel Matos <g.matos@outlook.com>
2022-01-11 22:29:34 -0300

    edita home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import ListTasks from '../../components/structure/ListTasks';
 const Home = () => {
   return(
     <div className="container">
-      <h1 className="text-center h1 mt-3">to-do list</h1>
+      <h1 className="text-center h1 mt-4 mb-4">To-do list</h1>
       <ListTasks/>
     </div>
   )
